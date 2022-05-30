9bd944b88c4df5ec5c403d7f825950ff3e5e877c
Eicy <32791896+cneicy@users.noreply.github.com>
2022-05-30 22:46:02 +0800

    i will update it ony day

---
+++ pi.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class pi {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("请输入圆周率小数位数");
+        String str1 = scan.next();
+        if(str1.equals("0")){
+            System.out.println("3");
+        }
+        if(str1.equals("1")){
+            System.out.println("3.1");
+        }
+        if(str1.equals("2")){
+            System.out.println("3.14");
+        }
+        if(str1.equals("3")){
+            System.out.println("3.141");
+        }
+        if(str1.equals("4")){
+            System.out.println("3.1415");
+        }
+        if(str1.equals("5")){
+            System.out.println("3.14159");
+        }
+        if(str1.equals("6")){
+            System.out.println("3.141592");
+        }
+        if(str1.equals("7")){
+            System.out.println("3.1415926");
+        }
+        if(str1.equals("8")){
+            System.out.println("3.14159265");
+        }
+        if(str1.equals("9")){
+            System.out.println("3.141592653");
+        }
+        if(str1.equals("10")){
+            System.out.println("3.1415926535");
+        }
+        if(str1.equals("11")){
+            System.out.println("3.14159265358");
+        }
+        if(str1.equals("12")){
+            System.out.println("3.141592653589");
+        }
+        if(str1.equals("13")){
+            System.out.println("3.1415926535897");
+        }
+        if(str1.equals("14")){
+            System.out.println("3.14159265358979");
+        }
+    }
+}
